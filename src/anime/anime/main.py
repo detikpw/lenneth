f00f0a59890aea3fabe612263f45ac66ba5fb65f
Detik <the2ndflamingheart@gmail.com>
2018-08-05 14:10:33 +0700

    feat(anime-cli): Create anime title question

--- src/anime/anime/main.py
+++ src/anime/anime/main.py
@@ -3,6 +3,8 @@ import requests
 import pprint
 import json
 import datetime
+from PyInquirer import prompt, style_from_dict, Token, Separator
+from toolz.curried import map
 from pkg.queries import ANIME
 """
 Module Anime
@@ -16,6 +18,7 @@ def main():
     from pyfiglet import Figlet
     f = Figlet(font='slant')
     print(f.renderText('ANIME-CLI'))
+
     now = datetime.datetime.now()
     variables = {
         'media_season': 'SUMMER',
@@ -27,8 +30,33 @@ def main():
 
     # Make the HTTP Api request
     response = requests.post(url, json={'query': ANIME, 'variables': variables})
+
+    get_title = lambda anime: anime['title']['romaji']
+    anime_titles = map(get_title)(response.json()['data']['Page']['media'])
+
+    style = style_from_dict({
+        Token.Separator: '#6C6C6C',
+        Token.QuestionMark: '#FF9D00 bold',
+        #Token.Selected: '',  # default
+        Token.Selected: '#5F819D',
+        Token.Pointer: '#FF9D00 bold',
+        Token.Instruction: '',  # default
+        Token.Answer: '#5F819D bold',
+        Token.Question: '',
+    })
+    questions = [
+        {
+            'type': 'list',
+            'name': 'anime_title',
+            # TODO remove this hard code
+            'message': 'This is top ten anime in summer 2018, which anime that you want to see detail?',
+            'choices': anime_titles
+        },
+    ]
+
+    answers = prompt(questions, style=style)
     pp = pprint.PrettyPrinter(indent=1)
-    pp.pprint(response.json()['data']['Page']['media'])
+    pp.pprint(answers)
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
